d1898b4bdb676acfd031157552e9d879ef1e8f8e
unknown <you@example.com>
2025-03-19 22:43:50 +0700

    ndb

---
+++ ndblesson14-api/ndb-lesson14-api/src/NdbApp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NdbApp(){
+  return(
+    <div className='container border my-3 p-3'>
+      <hl>React - API - Nguyễn Đình Báu</hl>
+    </div>
+    
+  )
+}
---
+++ ndblesson14-api/ndb-lesson14-api/src/components/NdbNavBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NdbReadApiLocal(){
+    return(
+        <div>
+            <h2>Đọc dữ liệu từ API Local</h2>
+        </div>
+    )
+}
